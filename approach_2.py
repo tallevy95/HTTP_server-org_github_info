42eef6ce5406e825f87f504c99f69f00a7474c2c
tallevy95 <77324590+tallevy95@users.noreply.github.com>
2023-08-18 15:11:20 +0300

    Update approach_2.py

--- approach_2.py
+++ approach_2.py
@@ -1,12 +1,13 @@
 import sys
-from flask import Flask, json
+from flask import Flask, json, request
 import requests
 
 app = Flask(__name__)
 
 # --------- GLOBALS ---------
 ORG_NAME = sys.argv[1]  # GitHub organization name
-URL = "https://api.github.com/orgs/%s/repos"  # When %s will be the organization name
+# ORG_NAME = "gofireflyio"
+URL = "https://api.github.com/orgs/%s/repos?page=%d"  # When %s will be the organization name
 
 
 # --------- functions ---------
@@ -22,10 +23,28 @@ def get_data(repository):
     return data
 
 
+def pagination_handler(repos, phrase):
+    """
+    This function handel one page only
+    :param repos: response.json() of the repository
+    :param phrase: a given phrase
+    :return: a list of the requested information
+    """
+    matching_repos = []
+
+    for repo in repos:
+        if phrase is not None and phrase in repo["name"].lower():
+            temp_dic = get_data(repo)
+        elif phrase is None:
+            temp_dic = get_data(repo)
+        else:  # for the case that we have phrase but don't need to show it
+            continue
+        matching_repos.append(temp_dic)
+    return matching_repos
+
+
 @app.route("/")
 def get_repositories():
-    url = URL % ORG_NAME
-    response = requests.get(url)
     try:
         if len(sys.argv[2]) >= 3:
             phrase = sys.argv[2]
@@ -34,18 +53,26 @@ def get_repositories():
     except:
         phrase = None
 
+    page = int(request.args.get("page", 1))  # Get the page number from query parameter, default to 1
+    url = URL % (ORG_NAME, page)
+    response = requests.get(url)
+
     if response.status_code == 200:
-        repos = response.json()
-        matching_repos = []
-        for repo in repos:
-            if phrase is not None and phrase.lower() in repo["name"].lower():
-                temp_dic = get_data(repo)
-            elif phrase is None:    # for the case that all repositories shuold be appear
-                temp_dic = get_data(repo)
-            else:   # for the case that we have phrase but don't need to show it
-                continue
-            matching_repos.append(temp_dic)
-        formatted_json = json.dumps(matching_repos, indent=4)  # Format the JSON with indentation
+        all_repos = []  # To store repositories from all pages
+        while response.status_code == 200:  # Continue while there are more pages
+            repos = response.json()
+            all_repos.append(repos)
+
+            # Check if there's another page
+            if 'next' in response.links:
+                url = response.links['next']['url']
+                response = requests.get(url)
+            else:
+                break   # if no other pages
+
+        data = *map(pagination_handler, all_repos, [phrase] * len(all_repos)),
+
+        formatted_json = json.dumps(data, indent=4)  # Format the JSON with indentation
         data = app.response_class(
             response=formatted_json,        # the json formatted data
             status=200,                     # OK
